d7c97babc50a7a339a8364b205c7582fa770bcf3
harsh-7991 <jadhavharshad89@gmail.com>
2025-01-23 22:08:00 +0530

    up 7

--- api/index.py
+++ api/index.py
@@ -1,35 +1,35 @@
-# from flask import *
+from flask import *
 
-
-# app = Flask(__name__)
-
-
-
-# # Index Page
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-
-
-
-
-
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+
+# Index Page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
+
+# @app.route('/about')
+# def about():
+#     return 'About'
