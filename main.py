9d93ad03de64eee097a5f92cb5feddefcda2aa20
harsh-7991 <jadhavharshad89@gmail.com>
2025-01-23 21:33:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import *
+
+
+app = Flask(__name__)
+
+
+
+
+# Index Page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
